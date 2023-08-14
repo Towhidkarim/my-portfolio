30d75d6aaae613996d50264e1250dc81fb10c940
Towhidkarim <towhidkarim123@gmail.com>
2023-08-14 13:48:28 +0600

    modified:   app/favicon.ico 	modified:   app/layout.tsx 	new file:   favicon.ico 	new file:   thumbnail.jpeg

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,13 +2,34 @@ import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import LoadingBar from './components/Animable/LoadingBar';
+import favicon from '@/assets/favicon.ico';
 import AnimatedCursor from './components/Animable/AnimatedCursor';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Towhid Karim | Portfolio',
-  description: "Towhid Karim's portfolio",
+  description:
+    "I'm a web developer and welcome to my portfolio, where I invite you to explore the culmination of my dedication and passion.",
+  keywords: [
+    'web',
+    'web developer',
+    'front-end',
+    'react',
+    'next.js',
+    'tailwind css',
+    'html',
+    'css',
+    'javascript',
+    'portfolio',
+  ],
+  robots: 'index, follow',
+  openGraph: {
+    title: 'Towhid Karim | Portfolio',
+    description:
+      'welcome to my portfolio, where I invite you to explore the culmination of my dedication and passion.',
+    images: '/thumbnail.jpeg',
+  },
 };
 
 export default function RootLayout({
@@ -18,7 +39,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
-      {/* <LoadingBar /> */}
+      {/* <head>
+        <link
+          rel='shortcut icon'
+          href='favicon.ico'
+          type='image/x-icon'
+          sizes='any'
+        />
+      </head> */}
       <body className='bg-base-100'>
         <LoadingBar />
         {/* <AnimatedCursor /> */}
